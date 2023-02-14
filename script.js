ce08a14d9e26dc86e18609a62b075f9e958d039a
Parschhurra Zimero <119706024+pzimero@users.noreply.github.com>
2023-02-14 13:18:19 -0500

    adding repos to the user card

--- script.js
+++ script.js
@@ -9,10 +9,21 @@ async function getUser(username) {
     const { data } = await axios(APIURL + username)
 
     createUserCard(data)
+    getRepos(username)
   } catch (err) {
     if (err.response.status == 404) {
       createErrorCard('This username does not exist')
     }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + '/repos?sort=created')
+
+    addReposToCard(data)
+  } catch (err) {
+    createErrorCard('Problem fetching repos')
   }
 }
 
@@ -50,6 +61,20 @@ function createErrorCard(msg) {
     `
 
   main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos')
+
+  repos.slice(0, 8).forEach((repo) => {
+    const repoEl = document.createElement('a')
+    repoEl.classList.add('repo')
+    repoEl.href = repo.html_url
+    repoEl.target = '_blank'
+    repoEl.innerText = repo.name
+
+    reposEl.appendChild(repoEl)
+  })
 }
 
 form.addEventListener('submit', (e) => {
